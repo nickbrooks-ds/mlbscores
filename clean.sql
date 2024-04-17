1c8b882374fdde15a2bf0c5ffb1796a25055e758
nickbrooks-ds <157837101+nickbrooks-ds@users.noreply.github.com>
2024-04-16 18:46:00 -0700

    Update clean.sql

--- clean.sql
+++ clean.sql
@@ -6,4 +6,10 @@ SELECT raw_json -> 'body' -> jsonb_object_keys(raw_json -> 'body') ->> 'away' AS
     (raw_json -> 'body' -> jsonb_object_keys(raw_json -> 'body') -> 'lineScore' -> 'home' ->> 'R')::INT AS home_team_score
 FROM scraper;
 
+INSERT INTO mlbscores2
+SELECT away_team, home_team, gameid, away_team_score, home_team_score,
+CONCAT(month, '/', day, '/', year)::DATE AS gamedate FROM 
+	(SELECT away_team, home_team, gameid, away_team_score, home_team_score,
+   substring(gameid, 5,2) AS month, substring(gameid, 7,2) as day, substring(gameid, 1,4) as year FROM mlbscores);
+
 DELETE FROM scraper;
